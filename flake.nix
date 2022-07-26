fb2be94283cc7ed9e9106462d4e76b5e8f0d9780
Domen Kožar <domen@dev.si>
2022-07-25 19:43:03 -0500

    simplify logic with cachix-deploy-lib

--- flake.nix
+++ flake.nix
@@ -5,9 +5,10 @@
     nixpkgs.url = "github:NixOS/nixpkgs/release-22.05";
     nixos-hardware.url = "github:nixos/nixos-hardware";
     cachix.url = "github:cachix/cachix/master";
+    cachix-deploy-flake.url = "github:cachix/cachix-deploy-flake";
   };
 
-  outputs = { self, nixpkgs, nixos-hardware, cachix }:
+  outputs = { self, nixpkgs, nixos-hardware, cachix, cachix-deploy-flake }:
     let
       system = "x86_64-linux";
       pkgs = import "${nixpkgs}" {
@@ -15,18 +16,18 @@
         # ngrok, vscode, zoom-us, signal-desktop
         config.allowUnfree = true;
       };
-      cherimoya = pkgs.nixos {
-        imports = [ 
-          ./cherimoya 
-          (nixos-hardware + "/lenovo/thinkpad/p14s/amd/gen2") 
-        ];
-        services.cachix-agent.package = import cachix { inherit system; };
-      };
+      cachix-deploy-lib = cachix-deploy-flake.lib pkgs;
     in {
-      defaultPackage."${system}" = pkgs.writeText "cachix-agents.json" (builtins.toJSON {
+      defaultPackage."${system}" = cachix-deploy-lib.spec {
         agents = {
-          cherimoya = cherimoya.toplevel;
+          cherimoya = cachix-deploy-lib.nixos {
+            imports = [
+              ./cherimoya 
+              (nixos-hardware + "/lenovo/thinkpad/p14s/amd/gen2") 
+            ];
+            services.cachix-agent.package = import cachix { inherit system; };
+          };
         };
-      });
+      };
     };
 }
