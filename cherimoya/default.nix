3673de794ca99d0904886360bf16dc80f072d04d
Domen Kožar <domen@dev.si>
2023-05-08 21:39:53 +0100

    enable p11-kit-proxy

--- cherimoya/default.nix
+++ cherimoya/default.nix
@@ -190,9 +190,15 @@
 
   # e-residency
   services.pcscd.enable = true;
+  programs.firefox.policies.SecurityDevices.p11-kit-proxy = "${pkgs.p11-kit}/lib/p11-kit-proxy.so";
   programs.firefox.nativeMessagingHosts.euwebid = true;
   environment.etc."chromium/native-messaging-hosts/eu.webeid.json".source = "${pkgs.web-eid-app}/share/web-eid/eu.webeid.json";
   environment.etc."opt/chrome/native-messaging-hosts/eu.webeid.json".source = "${pkgs.web-eid-app}/share/web-eid/eu.webeid.json";
+  # Tell p11-kit to load/proxy opensc-pkcs11.so, providing all available slots
+  # (PIN1 for authentication/decryption, PIN2 for signing).
+  environment.etc."pkcs11/modules/opensc-pkcs11".text = ''
+    module: ${pkgs.opensc}/lib/opensc-pkcs11.so
+  '';
 
   # TODO: need to hash the password
   #users.mutableUsers = false;
