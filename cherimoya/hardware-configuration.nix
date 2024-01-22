c286d5a072fba424fc8fbf6b68cbd49be16a346a
Domen Kožar <domen@dev.si>
2024-01-22 18:55:01 +0000

    use kernel 6.7

--- cherimoya/hardware-configuration.nix
+++ cherimoya/hardware-configuration.nix
@@ -14,7 +14,7 @@
   # prevent /boot from filling in
   boot.loader.grub.configurationLimit = 10;
 
-  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_6_6;
+  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_6_7;
 
   fileSystems."/" =
     { device = "/dev/disk/by-label/nixos";
