824fb9be21974222e536fcd9eb12d40afce2fdf7
Domen Kožar <domen@cachix.org>
2025-04-10 14:21:14 +0100

    bump kernel

--- cherimoya/hardware-configuration.nix
+++ cherimoya/hardware-configuration.nix
@@ -14,7 +14,7 @@
   # prevent /boot from filling in
   boot.loader.grub.configurationLimit = 10;
 
-  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_6_12;
+  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_6_14;
 
   fileSystems."/" =
     { device = "/dev/disk/by-label/nixos";
